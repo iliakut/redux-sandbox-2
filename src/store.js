e38a302d3052f6af41782b352fc0295c09686763
ilia <ilyakut@gmail.com>
2020-06-19 14:39:39 +0300

    added store.js enhancer

--- src/store.js
+++ src/store.js
@@ -1,4 +1,18 @@
 import {createStore} from "redux";
 import reducer from "./reducers/reducer";
 
-export const store = createStore(reducer);
+/*
+* 1. store создается с помощью createStore, в него передается reducer
+* 2. у createStore есть параметры:
+*   (reducer, initialState, enhancer)
+*   enhancer - что-то, что училивает работу store
+*   если enhancers несколько, то нужно использовать enhancer для их склеивания
+*   window.__REDUX_DEVTOOLS_EXTENSION__ - для работы расширения redux в devtools
+*
+*
+*/
+
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
